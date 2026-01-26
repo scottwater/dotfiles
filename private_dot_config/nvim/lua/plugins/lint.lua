d03b3177b761f0c9db8d26ea3c32ef576b0271b1
Scott Watermasysk <scottwater@gmail.com>
2026-01-26 11:56:41 -0500

    Manage lazyvim in Chezmoi and fix linting config issue

--- private_dot_config/nvim/lua/plugins/lint.lua
+++ private_dot_config/nvim/lua/plugins/lint.lua
@@ -1,12 +1,19 @@
 return {
   {
     "mfussenegger/nvim-lint",
-    opts = {
-      linters = {
-        ["markdownlint-cli2"] = {
-          args = { "--config", vim.fn.expand("~/.config/markdownlint-cli2.yaml"), "--" },
-        },
-      },
-    },
+    opts = function(_, opts)
+      local config_path = vim.fn.expand("~/.config/markdownlint-cli2.yaml")
+      local args = { "--" }
+      if vim.fn.filereadable(config_path) == 1 then
+        args = { "--config", config_path, "--" }
+      end
+
+      opts.linters = opts.linters or {}
+      opts.linters["markdownlint-cli2"] = {
+        args = args,
+      }
+
+      return opts
+    end,
   },
 }
